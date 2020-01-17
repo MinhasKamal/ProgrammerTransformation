9bd634b72e7ea28704938a30425337fac6366ef6
MinhasKamal <minhaskamal024@gmail.com>
2020-01-18 01:00:13 +0600

    We need git to upload our project to GitHub. BTW, why can't we upload it directly like GoogleDrive?

--- 06.PseudoSuperProgrammer.cpp
+++ 06.PseudoSuperProgrammer.cpp
@@ -1,6 +1,6 @@
-/// You understand that, mere comment cannot make
-/// bad code look good. So, you get rid of crappy
-/// names and comments, and make the code truly readable.
+/// You understand that, mere comment cannot make bad code look good.
+/// So, you get rid of those crappy names and comments, and make the
+/// code truly readable.
 
 bool primeNumberChecker(int number){
 
@@ -11,7 +11,7 @@ bool primeNumberChecker(int number){
 	if (number % 2 == 0){
 		return false;
 	}
-  
+
 	int limit = number/3;
 	for (int factor = 3; factor < limit; factor += 2){
 		if (number % factor == 0){
