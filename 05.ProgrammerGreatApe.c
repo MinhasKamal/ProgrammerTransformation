8603d17c005fc9a8f9342437fa79f08e2a0d200d
MinhasKamal <minhaskamal024@gmail.com>
2020-01-18 00:44:02 +0600

    I know what git does, still I don't understand why we need that shit.

--- 05.ProgrammerGreatApe.c
+++ 05.ProgrammerGreatApe.c
@@ -1,4 +1,4 @@
-/// You wish that, others may understand your code.
+/// You want everyone to understand your code easily.
 /// So, you put some REALLLLY helpful comments.
 
 /*
@@ -13,13 +13,13 @@ int prime_check(int n){
 		return 0;
 	}
 
-	int i = 3; // i is odd number
-	int l = n/3;
-	for (; i < l; i += 2){ // check for all odd numbers till n/3; increase i by 2
-		if (n % i == 0){ // if divisible by a number then not prime
+	int i = 3; // i is odd
+	int lim = n/3;
+	for (; i < lim; i += 2){ // check for all odd numbers till n/3
+		if (n % i == 0){ // if divisible then not prime
 			return 0; // not prime
 		}
 	}
 
-	return 1; // prime number
+	return 1; // is prime number
 }
