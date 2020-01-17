7097f204aaa550775e6f698650efdd038b5816ee
MinhasKamal <minhaskamal024@gmail.com>
2020-01-17 23:51:41 +0600

    Somewhere I have heard this word 'git'... Cannot remember :(

--- 02.ProgrammerKaioken4.c
+++ 02.ProgrammerKaioken4.c
@@ -1,11 +1,12 @@
 /// You have learned to use flag, and think
-/// that- it is the coolest.
+/// that- it is the coolest. Also, indentation
+/// is super important!
 
-int func_pri_num(int a){
-	int i, f = 0;
-	for (i = 2; i < a; i++)
+int pri_num(int a) {
+	int i = 0, f = 0;
+	for (i = 2; i < a; i ++)
 		if (a % i == 0) f = 1;
-	
+
 	if (f == 0) return 1;
 	else return 0;
 }
