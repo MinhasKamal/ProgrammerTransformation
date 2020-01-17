c58117ddcd1a0296fd3a5ad12566c430cf9af818
MinhasKamal <minhaskamal024@gmail.com>
2020-01-18 00:35:05 +0600

    Git is used for controlling code version. I know that!

--- 04.ProgrammerKaioken20.c
+++ 04.ProgrammerKaioken20.c
@@ -1,6 +1,6 @@
-/// You have grown smarter, and cut half of the run time
-/// by checking divisibility with only odd-numbers.
-/// You also understand that looping till x/2 is enough.
+/// You have grown SMARTER, and cut half of the run time
+/// by checking divisibility with odd-numbers only. You
+/// also understand that looping till x/2 is enough.
 
 int prime_or_not(int x) {
 	if (x == 2) {
