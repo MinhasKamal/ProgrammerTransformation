eba128d3a4593c95d676494dc3777957ba098dd5
MinhasKamal <minhaskamal024@gmail.com>
2020-01-17 23:43:28 +0600

    git! what the damn hell?

--- 01.BaseProgrammer.c
+++ 01.BaseProgrammer.c
@@ -1,20 +1,21 @@
-/// It took a long time to come up with this solution.
-/// You made several mistakes at the beginning. But now,
-/// you are proud of your code.
+/// Though you knew how prime number worked, it took a long time
+/// to come up with this logic. Also, You made several mistakes
+/// on the way. And now, you are proud of your brilliant solution.
 
-int func (int a)
-{
+int func(int a)   
+{ 
 	int x,i,r;
-	x=0;
-	i=0;
-	for (i=1; i<=a; i=i+1)
-	{
-		if (a%i==0)
-			x=x+1;
+	x = 0 ;
+	i = 0 ;
+	for(i=1; i<=a ; i=i+1 )
+	 {
+		if (a%i == 0)
+		  	{x=x+1 ;}
 	}
-	if (x==2)
-		r=1;
+	if( x == 2)
+		{r =  1;}
 	else
-		r=0;
-	return r;
+		{r = 0;}
+	return r ;  
+ 
 }
