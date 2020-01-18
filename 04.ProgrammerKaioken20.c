990621548780cde52b0df8c03c1454eefccfd9a2
MinhasKamal <minhaskamal024@gmail.com>
2020-01-18 10:31:37 +0600

    We need git to upload our project to GitHub. BTW, why can't we upload it directly like GoogleDrive?

--- 04.ProgrammerKaioken20.c
+++ 04.ProgrammerKaioken20.c
@@ -1,6 +1,6 @@
-/// You have grown SMARTER, and cut half of the run time
-/// by checking divisibility with odd-numbers only. You
-/// also understand that looping till x/2 is enough.
+/// You have grown SMARTER, and halved the run time by checking
+/// divisibility with odd-numbers only. You also understand that
+/// looping till x/2 is enough.
 
 int prime_or_not(int x) {
 	if (x == 2) {
