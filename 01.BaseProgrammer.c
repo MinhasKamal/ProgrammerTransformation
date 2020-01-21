25659488ebad8890f83a87956b9604ba44317407
MinhasKamal <minhaskamal024@gmail.com>
2020-01-21 23:23:44 +0600

    git! what the damn hell?

--- 01.BaseProgrammer.c
+++ 01.BaseProgrammer.c
@@ -4,18 +4,18 @@
 
 int func(int a)   
 { 
-	int x,i,r;
-	x = 0 ;
+int x,i, r;
+	x = 0;
 	i = 0 ;
-	for(i=1; i<=a ; i=i+1 )
-	 {
-		if (a%i == 0)
-		  	{x=x+1 ;}
-	}
-	if( x == 2)
-		{r =  1;}
-	else
-		{r = 0;}
-	return r ;  
- 
+for(i=1; i<=a ; i=i+1 )
+	{
+	if(a%i == 0)
+	{x=x+1 ;}
+	 }
+if( x == 2)
+	{r =  1;}
+else
+	{r = 0;}
+ return r ;  
+
 }
