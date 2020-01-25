dd90b78721da2660b5ae714eb7815bcd9a6a9f45
MinhasKamal <minhaskamal024@gmail.com>
2020-01-25 11:54:14 +0600

    -_-

--- 11.SuperProgrammer4.java
+++ 11.SuperProgrammer4.java
@@ -1,7 +1,8 @@
 /// u don't give a f*$%
+/// Explanation: https://stackoverflow.com/questions/2795065/how-to-determine-if-a-number-is-a-prime-with-regex
 
 public class PrimeNumberChecker {
     public boolean isPrime(int n) {
-        return n>1?!new String(new char[n]).matches(".?|(..+?)\\1+"):false;
+        return n>1? !new String(new char[n]).matches(".?|(..+?)\\1+"): false;
     }
 }
