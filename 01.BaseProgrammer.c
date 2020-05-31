df438d3cd2f6fec418324a094632d1d6998bdbab
Minhas Kamal <minhaskamal024@gmail.com>
2020-05-31 22:50:11 +0600

    Git? A strain of insect I presume.

--- 01.BaseProgrammer.c
+++ 01.BaseProgrammer.c
@@ -1,6 +1,7 @@
-/// Though you knew how prime number worked, it took a long time
-/// to come up with this logic. Also, You made several mistakes
-/// on the way. And now, you are proud of your brilliant solution.
+/// Though u knew how prime number works (of course u r good @ math),
+/// it took a long time to come up with the logic. Also, u made several
+/// mistakes on the way. And now, u are proud of ur brilliant solution.
+/// YES!!!
 
 int func(int a)   
 { 
@@ -11,11 +12,11 @@ for(i=1; i<=a ; i=i+1 )
 	{
 	if(a%i == 0)
 	{x=x+1 ;}
-	 }
+	}
 if( x == 2)
 	{r =  1;}
 else
 	{r = 0;}
- return r ;  
+        return r ;  
 
 }
